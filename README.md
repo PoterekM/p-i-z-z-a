dba4210d67c7c41745f666363b7907e4e237ee2d
Michelle Poterek <poterekm@gmail.com>
2017-06-16 16:14:09 -0700

    test to push code

---
+++ README.md
@@ -0,0 +1,50 @@
+# _Olsen Twin's Pizza_
+
+#### _Epicodus Intro to Programming: Object Oriented Independent Project, June 16, 2017_
+
+#### By _**Michelle Poterek**_
+
+## Description
+
+_This project allows a user to order a theoretical Mary Kate and Ashley pizza!_
+* **Users are able to select a size**
+* **Users are able to pick their toppings**
+* **Users are able to see the price of their pizza**
+
+
+
+
+![Chart of Behaviors for program to run without bugs](img/BDD.png)
+
+## Setup/Installation Requirements
+
+_In order to copy this directory to your computer locally:_
+* _open Terminal and type:_
+* _$ git clone `https://github.com/PoterekM/ping-pong.git`_
+* _$ cd `ping-pong`_
+* _$ open `index.html`_
+
+To access this repository remotely click this link: <br>
+https://poterekm.github.io/ping-pong/
+
+<!-- * _This is a great place_
+* _to list setup instructions_
+* _in a simple_
+* _easy-to-understand_
+* _format_ -->
+<!-- _{Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. Do I need to run a server? How should I set up my databases? Is there other code this app depends on?}_ -->
+
+
+## Support and contact details
+
+_Please feel free to contact me directly via e-mail at poterekm@gmail.com if you have any questions, comments, ideas, or feedback. Also, I invite you to feel empowered to make any changes to this repository by forking it and making changes accordingly._
+
+## Technologies Used
+
+_This project utilizes jQuery (version 3.2.1) and Bootstrap for styling._
+
+### License
+
+*This project is under the MIT License*
+
+Copyright (c) 2017 **Michelle Poterek**
---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+// Business Logic
+function Pizza (size, topping) {
+  this.size = size;
+  this.topping = topping;
+}
+
+
+Pizza.prototype.calcTotal = function() {
+  console.log("hey");
+  var total = 0;
+  if (this.size === "small") {
+    total += 12
+    console.log("hey");
+  } else if (this.size === "medium") {
+    total += 18
+  } else if (this.size === "large") {
+    total += 24
+  } else if (this.size === "mondo") {
+    total += 60
+  } else {
+    total = 66
+    alert("uhhh... do you want us to clean out our kitchen for you or not? Ya need to pick a size in order for us to make this.")
+  }
+
+
+  // Calculate based number of toppings
+   for (var index = 0; index < this.topping.length; index ++) {
+       total += 3;
+     }
+     return total;
+   };
+
+
+// TO BE CONTINUED LATER
+// function Customer (name, address){
+//   this.name = name;
+//   this.address = address;
+// }
+
+
+// Pizza.prototype.displayPizza.function() {
+//   return "You have chosen a" + this.size + "pizza";
+// }
+
+
+
+// User Interface Logic
+$(document).ready(function() {
+  $("#pizzaSelection").submit(function(event){
+    event.preventDefault();
+
+    var inputSize = $("input:radio[name=size]:checked").val();
+    var userToppings = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      userToppings.push($(this).val());
+    });
+    // var totalAmount = this.total;
+    // console.log(totalAmount);
+    console.log(userToppings);
+    console.log(inputSize);
+    var newPizza = new Pizza(inputSize, userToppings, Pizza.calcTotal);
+
+    // console.log(newPizza.size); // confirm upstairs is talking to downstairs by giving new object plus the constructor property
+
+
+    $("#user-size").text(newPizza.size);
+    $("#user-toppings").text(newPizza.topping);
+    $("#user-total").text("$" + newPizza.calcTotal());
+  });
+});
